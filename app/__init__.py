4e852168c173bd2e7b57c9a6a8e072eb7f9986d8
twilight@solfoto.ru <twilight@solfoto.ru>
2018-04-13 17:10:48 +0300

    The process of launching the application was changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,29 @@
 # -*- coding: utf-8 -*-
+
+import connexion
+import flask
+import gevent.monkey
+
+from connexion.resolver import RestyResolver
+from flask_injector import FlaskInjector
+from time import sleep
+
+
+gevent.monkey.patch_all()
+
+
+def get_greeting():
+    def generator():
+        while True:
+            yield 'beat\n\n'
+            sleep(5)
+    response = flask.Response(generator(), mimetype='text/plain', status=200)
+    return response
+
+
+def main():
+    # app = connexion.App(__name__, server='gevent', specification_dir='swagger/')
+    app = connexion.App(__name__, specification_dir='swagger/')
+    app.add_api('indexer.yaml', resolver=RestyResolver('app.api'), arguments={'title': 'Sample Ice Chart Service'})
+    FlaskInjector(app=app.app)
+    app.run(port=5000)
